0df4c39c0a415e73fe6964f7fddc70c7dde6965d
MileneGeraldo98 <122188564+MileneGeraldo98@users.noreply.github.com>
2023-06-03 23:47:07 -0300

    Exec em JS

---
+++ ex008.js
@@ -0,0 +1,8 @@
+var vel = 15
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { // Condição simples
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO !`)
+}
+
+
+console.log(`Dirija sempre usando o cinto de segurança !`)
